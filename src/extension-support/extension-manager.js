931b0e247ee218dcf5f958d2ca70eb2fa6b56a01
Lucien Murray-Pitts <lucienmp_antispam@yahoo.com>
2025-09-21 19:49:54 +0900

    Adding spotpear C3 1.44inch minitv board support for block elements

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -63,6 +63,8 @@ const builtinDevices = {
     arduinoRaspberryPiPico2: () => require('../devices/arduinoRaspberryPiPico2/arduinoRaspberryPiPico2'),
     // Raspberry Pi Pico 2W
     arduinoRaspberryPiPico2W: () => require('../devices/arduinoRaspberryPiPico2W/arduinoRaspberryPiPico2W'),
+    // SpotPear
+    spotpear: () => require('../devices/spotpear/spotpear'),
     // Microbit
     microbit: () => require('../devices/microbit/microbit'),
     microbitV2: () => require('../devices/microbit/microbitV2')
